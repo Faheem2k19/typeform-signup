db4817a197c6bb163eeaf4c0b4c28cc02ba76bdc
Faheem2k19 <asifulfahim04@gmail.com>
2024-03-26 04:40:04 +0600

    updated

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -38,7 +38,7 @@ export default function Header() {
                             <path d="M10 0C4.5 0 0 4.5 0 10s4.5 10 10 10 10-4.5 10-10S15.5 0 10 0zM9 17.9C5.1 17.4 2 14 2 10c0-.6.1-1.2.2-1.8L7 13v1c0 1.1.9 2 2 2v1.9zm6.9-2.5c-.3-.8-1-1.4-1.9-1.4h-1v-3c0-.6-.4-1-1-1H6V8h2c.6 0 1-.4 1-1V5h2c1.1 0 2-.9 2-2v-.4c2.9 1.2 5 4.1 5 7.4 0 2.1-.8 4-2.1 5.4z" fill="#5E5E5E" fillRule="evenodd" />
                         </svg>
                         <span className="text-sm font-sans">&nbsp;{language}</span>
-                        <svg className={`ml-1 transition-transform duration-150 ease-in ${showDropdown ? 'rotate-180' : ''}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width={14} height={14}>
+                        <svg className="ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width={14} height={14}>
                             <path fillRule="evenodd" d="M10 12.12l-5.12-5.12a.75.75 0 111.06-1.06L10 10l4.06-4.06a.75.75 0 111.06 1.06L10 12.12z" clipRule="evenodd" />
                         </svg>
                     </button>
